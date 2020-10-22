6b732dcd0cfdf3a74aebee466f915d61383e3d62
Gustavo-luchesi <gustavo_luchesi@outlook.com>
2020-10-22 09:15:50 -0300

    Coloquei a base com indicadores sociais do IBGE 2010

--- Script_1.R
+++ Script_1.R
@@ -24,19 +24,19 @@ library(broom)
 # Base de dados com o PIB Municipal
 
 base_PIB <- read_excel("PIB_2010_2017.xlsx")%>% 
-  rename(codigo_regiao = "Código da Grande Região", 
-         regiao = "Nome da Grande Região",
-         codigo_UF = "Código da Unidade da Federação",
-         UF = "Sigla da Unidade da Federação",
-         nome_UF = "Nome da Unidade da Federação",
-         codigo_municipio = "Código do Município",
-         nome_municipio = "Nome do Município",
-         regiao_metropolitana = "Região Metropolitana",
-         codigo_mesorregiao = "Código da Mesorregião",
-         nome_mesorregiao = "Nome da Mesorregião",
-         codigo_microrregiao = "Código da Microrregião",
-         nome_microrregiao =  "Nome da Microrregião",
-         PIB = "Produto Interno Bruto, \r\na preços correntes\r\n(R$ 1.000)") %>%
+  rename(codigo_regiao = "C?digo da Grande Regi?o", 
+         regiao = "Nome da Grande Regi?o",
+         codigo_UF = "C?digo da Unidade da Federa??o",
+         UF = "Sigla da Unidade da Federa??o",
+         nome_UF = "Nome da Unidade da Federa??o",
+         codigo_municipio = "C?digo do Munic?pio",
+         nome_municipio = "Nome do Munic?pio",
+         regiao_metropolitana = "Regi?o Metropolitana",
+         codigo_mesorregiao = "C?digo da Mesorregi?o",
+         nome_mesorregiao = "Nome da Mesorregi?o",
+         codigo_microrregiao = "C?digo da Microrregi?o",
+         nome_microrregiao =  "Nome da Microrregi?o",
+         PIB = "Produto Interno Bruto, \r\na pre?os correntes\r\n(R$ 1.000)") %>%
   filter(Ano == 2017) %>% 
   select(regiao, UF, 
          nome_municipio, 
@@ -114,7 +114,7 @@ mais65 <- age %>%
 ## Dados sobre mortalidade
 #===========================================================================================
 
-## Caso não tenha os dados no sistema 
+## Caso n?o tenha os dados no sistema 
 #===========================================================================================
 
 SIH_18 <- fetch_datasus(year_start = 2018, month_start = 1, year_end = 2018, month_end = 8, information_system = "SIH-RD")
@@ -160,7 +160,7 @@ load("SIH_20.Rdata")
 obitos <- SIH_18 %>% 
   left_join(SIH_19, by = c('MES_CMPT' = 'MES_CMPT', 'MUNIC_RES' = 'MUNIC_RES')) %>% 
   left_join(SIH_20, by = c('MES_CMPT' = 'MES_CMPT', 'MUNIC_RES' = 'MUNIC_RES')) %>% 
-  filter(MES_CMPT != 8) #ainda não temos dados completos de agosto
+  filter(MES_CMPT != 8) #ainda n?o temos dados completos de agosto
 
 mortes <- obitos %>%
   mutate(MES_CMPT = as.character(MES_CMPT)) %>% 
@@ -292,11 +292,11 @@ COVID %>%
     geom_line(aes(MES_CMPT, excesso, group = wealth, color = wealth), size = 2) +
   theme_classic() +
   ylab("Excesso de mortalidade relativo") + 
-  xlab("Mês") +
-  scale_colour_discrete(name = "Nível de PIB per capita", labels = c("Alto","Médio", "Baixo")) +
-  labs(color = "Nível de Renda",
-       title = "Evolução do excesso de mortalidade em 2020",
-       subtitle = "por microrregião",
+  xlab("M?s") +
+  scale_colour_discrete(name = "N?vel de PIB per capita", labels = c("Alto","M?dio", "Baixo")) +
+  labs(color = "N?vel de Renda",
+       title = "Evolu??o do excesso de mortalidade em 2020",
+       subtitle = "por microrregi?o",
        caption = "Fonte: MicroDataSUS e IBGE")
 
 COVID %>%
@@ -308,11 +308,11 @@ COVID %>%
   geom_line(aes(MES_CMPT, excesso, group = wealth, color = wealth), size = 2) +
   theme_classic() +
   ylab("Excesso de mortalidade relativo") + 
-  xlab("Mês") + 
+  xlab("M?s") + 
   coord_cartesian(xlim = c(1,7), ylim = c(0,1.5))+
-  scale_colour_discrete(name = "Nível de PIB per capita", labels = c("Alto","Médio", "Baixo"))+
-  labs(title = "Evolução do excesso de mortalidade em 2020",
-       subtitle = "por microrregião",
+  scale_colour_discrete(name = "N?vel de PIB per capita", labels = c("Alto","M?dio", "Baixo"))+
+  labs(title = "Evolu??o do excesso de mortalidade em 2020",
+       subtitle = "por microrregi?o",
        caption = "Fonte: MicroDataSUS e IBGE")
 
 
@@ -369,8 +369,6 @@ covid_bruto <- read_excel("~/Desktop/Insper Data/HIST_PAINEL_COVIDBR_31ago2020_1
                                                                                                      "text", "text","numeric","numeric","numeric",
                                                                                                      "text", "date","numeric","numeric","numeric","numeric",
                                                                                                      "numeric","numeric","numeric","numeric","logical"))
-
-
 covid_mensal <- covid_bruto %>%
   filter(codmun != 0, 
          codRegiaoSaude != 0,
@@ -466,7 +464,7 @@ SIH_20 <- SIH_20 %>%
 obitos <- SIH_18 %>% 
   left_join(SIH_19, by = c('MES_CMPT' = 'MES_CMPT', 'MUNIC_RES' = 'MUNIC_RES')) %>% 
   left_join(SIH_20, by = c('MES_CMPT' = 'MES_CMPT', 'MUNIC_RES' = 'MUNIC_RES')) %>% 
-  filter(MES_CMPT != 8) #ainda não temos dados completos de agosto
+  filter(MES_CMPT != 8) #ainda n?o temos dados completos de agosto
 
 
 
@@ -653,12 +651,36 @@ COVID <- excesso_f %>%
 
 lm(excesso_mortes ~ Riqueza + populacao_total + mais65 + codSaude, data = COVID)
 
+#====================================================================
+#Indicadores sociais IBGE 2010
+library(readxl)
+IDH_2010 <- read_excel("~/Desktop/IDH_2010.xls")
 
-#
-#
-#
-#
-#
-#
-#
+CENSO_2010 <- IDH_2010 %>% 
+  rename(COD_MUN = `CÃ³digo do MunicÃ­pio`,
+         EXP_EST = `Expectativa de anos de estudo`,
+         ANALF_15mais = `Taxa de analfabetismo - 15 anos ou mais`,
+         GINI = `Ãndice de Gini`,
+         EXTR_POBR = `% de extremamente pobres`,
+         TRAB_PROP = `% de trabalhadores por conta prÃ³pria - 18 anos ou mais`,
+         TAXA_ATIV_18mais = `Taxa de atividade - 18 anos ou mais`,
+         AGUA_ENC = `% da populaÃ§Ã£o em domicÃ­lios com Ã¡gua encanada`,
+         BANHEIRO = `% da populaÃ§Ã£o em domicÃ­lios com banheiro e Ã¡gua encanada`,
+         COLETA = `% da populaÃ§Ã£o em domicÃ­lios com coleta de lixo`,
+         ENERGIA = `% da populaÃ§Ã£o em domicÃ­lios com energia elÃ©trica`,
+         AGUA_INADEQUADO = `% de pessoas em domicÃ­lios com abastecimento de Ã¡gua e esgotamento sanitÃ¡rio inadequados`,
+         SUB_ESC = `SubÃ­ndice de escolaridade - IDHM EducaÃ§Ã£o`,
+         SUB_FREQ = `SubÃ­ndice de frequÃªncia escolar - IDHM EducaÃ§Ã£o`,
+         IDHM_Educ = `IDHM EducaÃ§Ã£o`,
+         IDHM_Long = `IDHM Longevidade`,
+         IDHM_Renda = `IDHM Renda`) %>% 
+  select(MunicÃ­pio, COD_MUN, EXP_EST, ANALF_15mais, GINI, EXTR_POBR, 
+         TRAB_PROP, TAXA_ATIV_18mais, AGUA_ENC, BANHEIRO,
+         COLETA, ENERGIA, AGUA_INADEQUADO, IDHM, SUB_ESC, SUB_FREQ,
+         IDHM_Educ, IDHM_Long, IDHM_Renda)
+
+
+#PNAD Covid
+
+
 
